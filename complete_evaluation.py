42058f6bb2decc4700085addc8c69f4e026c5335
Ravi Theja <ravi.theja@inmobi.com>
2023-04-01 15:28:56 +0530

    Add Evaluation code

--- complete_evaluation.py
+++ complete_evaluation.py
@@ -1,3 +1,4 @@
+```python
 from gpt_index import (
     GPTTreeIndex,
     GPTSimpleVectorIndex,
@@ -28,3 +29,19 @@ vector_index.save_to_disk('./simple_vector_index.json')
 
 vector_index = GPTSimpleVectorIndex.load_from_disk(
     './simple_vector_index.json')
+
+# Evaluation
+def evaluate_response(query: str, index, mode: str) -> str:
+    response = index.query(query)
+    evaluator = ResponseEvaluator(mode=mode, service_context=service_context_gpt3)
+    evaluation = evaluator.evaluate(response)
+    return evaluation
+
+query_str = "your query here"
+mode = "context_response"  # Choose the evaluation mode ("context_response" is the only available mode)
+
+evaluation_result = evaluate_response(query_str, vector_index, mode)
+print(f"Evaluation: {evaluation_result}")
+```
+
+In the code above, we import the necessary modules and initialize the GPT-3 predictor, load the data and gpt/index files, and create a helper function `evaluate_response` to perform the evaluation on a given query. The function takes a query string, an index (e.g., vector_index), and an evaluation mode as input and returns the evaluation result.
