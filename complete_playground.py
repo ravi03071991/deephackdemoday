5c9ef6b64e836e6add7f1d8e45efa1ace94d7557
Ravi Theja <ravi.theja@inmobi.com>
2023-04-01 15:51:01 +0530

    Add playground code

--- complete_playground.py
+++ complete_playground.py
@@ -1,37 +1,42 @@
 ```python
-# Import necessary libraries and modules
-import logging
-from llama_index import download_loader
-from llama_index.indices.vector_store import GPTSimpleVectorIndex
-from llama_index.indices.tree.base import GPTTreeIndex
-from gpt_index.indices.list.base import GPTListIndex
-from gpt_index.playground.base import Playground
-
 # Hide INFO logs regarding token usage, etc
+import logging
 logger = logging.getLogger()
 logger.setLevel(logging.CRITICAL)
 
-# Load data
+from llama_index import download_loader
+from llama_index.indices.vector_store import GPTSimpleVectorIndex
+from llama_index.indices.tree.base import GPTTreeIndex
+
+from gpt_index.playground.base import Playground
+from gpt_index.readers.schema.base import Document
+
+# Load the WikipediaReader
 WikipediaReader = download_loader("WikipediaReader")
 loader = WikipediaReader()
 documents = loader.load_data(pages=['Berlin'])
 
-# Create indices
-indices = [GPTSimpleVectorIndex.from_documents(documents),
-           GPTTreeIndex.from_documents(documents),
-           GPTListIndex.from_documents(documents)]
+# Create indices using the loaded documents
+indices = [
+    GPTSimpleVectorIndex.from_documents(documents),
+    GPTTreeIndex.from_documents(documents)
+]
 
-# Initialize the Playground with the created indices
+# Initialize Playground with the created indices
 playground = Playground(indices)
 
-# Query to compare
-query_text = "What is the population of Berlin?"
+# Define and compare a sample query
+query_text = "What is the capital of Germany?"
+comparison_result = playground.compare(query_text)
 
-# Compare the output of the indices
-result = playground.compare(query_text, to_pandas=True)
+# Print the comparison results
+print("\nComparison Results:")
+print(comparison_result)
 
-# Print the result
-print(result)
+# Run the Playground interactively (optional)
+# playground.interact()
 ```
 
-This playground code imports the necessary libraries and modules, loads the Wikipedia data for the page "Berlin", creates three different indices (GPTSimpleVectorIndex, GPTTreeIndex, and GPTListIndex) using the provided gpt-index/llama-index code base, initializes a Playground using these indices, and then uses the `compare` function of the Playground class to compare the outputs for a given query. The result is printed as a pandas DataFrame.
+In this code snippet, we first import the necessary modules and classes, such as `logging`, `download_loader`, `GPTSimpleVectorIndex`, `GPTTreeIndex`, and `Playground`. Then, we load the `WikipediaReader`, create the two index types using the loaded documents, and initialize the `Playground` with these indices.
+
+After that, we define a sample query and compare the outputs of the different indices. The comparison results are then printed. Optionally, you can uncomment the last line to run the Playground interactively.
