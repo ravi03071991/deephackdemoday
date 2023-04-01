c033f9399a26be14117431b3b8a573d76c9ed1b3
Ravi Theja <ravi.theja@inmobi.com>
2023-04-01 16:44:12 +0530

    Add chatgpt code to simple vector index

--- update_chatgpt.py
+++ update_chatgpt.py
@@ -1,43 +1,42 @@
 ```python
 from gpt_index import (
-    GPTTreeIndex,
     GPTSimpleVectorIndex,
     SimpleDirectoryReader,
     LLMPredictor,
     ServiceContext,
-    Response
+    ChatOpenAI
 )
-from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI
-from gpt_index.evaluation import ResponseEvaluator
 import pandas as pd
 pd.set_option('display.max_colwidth', 0)
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
-
 # gpt-3.5-turbo
-llm_predictor_gpt35 = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
-service_context_gpt35 = ServiceContext.from_defaults(llm_predictor=llm_predictor_gpt35, chunk_size_limit=512)
+llm_predictor_gpt35 = LLMPredictor(llm=ChatOpenAI(
+    temperature=0, model_name="gpt-3.5-turbo"))
+service_context_gpt35 = ServiceContext.from_defaults(
+    llm_predictor=llm_predictor_gpt35,
+    chunk_size_limit=512
+)
 
 documents = SimpleDirectoryReader('../test_wiki/data').load_data()
 
-tree_index = GPTTreeIndex.load_from_disk('../test_wiki/index.json')
-
-# create vector index with ChatGPT
-vector_index_chatgpt = GPTSimpleVectorIndex.from_documents(
+# create vector index for ChatGPT (GPTSimpleVectorIndex)
+chatgpt_vector_index = GPTSimpleVectorIndex.from_documents(
     documents,
     service_context=service_context_gpt35
 )
-vector_index_chatgpt.save_to_disk('../test_wiki/simple_vector_index_chatgpt.json')
+chatgpt_vector_index.save_to_disk('../test_wiki/chatgpt_simple_vector_index.json')
 
-vector_index_chatgpt = GPTSimpleVectorIndex.load_from_disk(
-    '../test_wiki/simple_vector_index_chatgpt.json')
+chatgpt_vector_index = GPTSimpleVectorIndex.load_from_disk(
+    '../test_wiki/chatgpt_simple_vector_index.json')
+
+# Example query for ChatGPT vector index
+response = chatgpt_vector_index.query(
+    "What is the significance of this document?",
+    service_context=service_context_gpt35,
+    similarity_top_k=3
+)
+
+print(response)
 ```
 
-The above code is used for creating an updated simple vector index chatgpt with gpt-3.5-turbo model.
-The changes are:
-1. Import necessary library 'sys' and set logging level to 'INFO'.
-2. Replace 'text-davinci-003' with the 'gpt-3.5-turbo' model_name in LLMPredictor instance creation.
-3. Set the `chunk_size_limit` for `service_context_gpt35` to 512.
-4. Create and save the updated simple vector index chatgpt with the new service context.
+In this code, I've updated the original `gpt-index/llama-index` code by using the `ChatOpenAI` module for setting up the `llm_predictor_gpt35` with `gpt-3.5-turbo` model. Then, I created a new `service_context_gpt35` with the `llm_predictor_gpt35`. The `GPTSimpleVectorIndex.from_documents` function is called with the `service_context_gpt35`, and the index is saved to disk as `chatgpt_simple_vector_index.json`. Finally, I showed an example query for the ChatGPT vector index by calling the `query` method with the new `service_context_gpt35`.
