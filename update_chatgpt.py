24ca4ae02b59908da9ae2b66481a76812d3b2c83
Ravi Theja <ravi.theja@inmobi.com>
2023-04-01 16:41:40 +0530

    Add chatgpt code

--- update_chatgpt.py
+++ update_chatgpt.py
@@ -1,5 +1,3 @@
-Here is the updated code for gpt-index/llama-index after incorporating the documentation provided:
-
 ```python
 from gpt_index import (
     GPTTreeIndex,
@@ -12,44 +10,34 @@ from gpt_index import (
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 from gpt_index.evaluation import ResponseEvaluator
-from llama_index import SimpleMongoReader
 import pandas as pd
 pd.set_option('display.max_colwidth', 0)
 
-# gpt-3 (davinci)
-llm_predictor_gpt3 = LLMPredictor(llm=OpenAI(
-    temperature=0, model_name="text-davinci-003"))
-service_context_gpt3 = ServiceContext.from_defaults(
-    llm_predictor=llm_predictor_gpt3)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+# gpt-3.5-turbo
+llm_predictor_gpt35 = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
+service_context_gpt35 = ServiceContext.from_defaults(llm_predictor=llm_predictor_gpt35, chunk_size_limit=512)
 
 documents = SimpleDirectoryReader('../test_wiki/data').load_data()
 
 tree_index = GPTTreeIndex.load_from_disk('../test_wiki/index.json')
 
-# create vector index
-vector_index = GPTSimpleVectorIndex.from_documents(
+# create vector index with ChatGPT
+vector_index_chatgpt = GPTSimpleVectorIndex.from_documents(
     documents,
-    service_context=ServiceContext.from_defaults(chunk_size_limit=512)
+    service_context=service_context_gpt35
 )
-vector_index.save_to_disk('../test_wiki/simple_vector_index.json')
+vector_index_chatgpt.save_to_disk('../test_wiki/simple_vector_index_chatgpt.json')
 
-vector_index = GPTSimpleVectorIndex.load_from_disk(
-    '../test_wiki/simple_vector_index.json')
-
-# Mongo Integration
-host = "<host>"
-port = "<port>"
-db_name = "<db_name>"
-collection_name = "<collection_name>"
-query_dict = {}
-reader = SimpleMongoReader(host, port)
-mongo_documents = reader.load_data(db_name, collection_name, query_dict=query_dict)
-
-# Updating index with Mongo documents
-tree_index.update_from_documents(mongo_documents)
-
-# Save updated index
-tree_index.save_to_disk('../test_wiki/updated_index.json')
+vector_index_chatgpt = GPTSimpleVectorIndex.load_from_disk(
+    '../test_wiki/simple_vector_index_chatgpt.json')
 ```
 
-In the above-updated code, I have added the necessary imports from the documentation and incorporated the MongoDB integration. The `SimpleMongoReader` is used to read documents from the specified MongoDB collection. Then, the tree index is updated with these Mongo documents using the `update_from_documents` method. Finally, the updated index is saved to disk. Remember to replace "<host>", "<port>", "<db_name>", "<collection_name>", and "<query_text>" with your own specific values.
+The above code is used for creating an updated simple vector index chatgpt with gpt-3.5-turbo model.
+The changes are:
+1. Import necessary library 'sys' and set logging level to 'INFO'.
+2. Replace 'text-davinci-003' with the 'gpt-3.5-turbo' model_name in LLMPredictor instance creation.
+3. Set the `chunk_size_limit` for `service_context_gpt35` to 512.
+4. Create and save the updated simple vector index chatgpt with the new service context.
