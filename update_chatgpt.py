b78773f5647645de4134b7d31811118f7782c552
Ravi Theja <ravi.theja@inmobi.com>
2023-04-01 16:37:06 +0530

    Add chatgpt code

--- update_chatgpt.py
+++ update_chatgpt.py
@@ -1,3 +1,5 @@
+Here is the updated code for gpt-index/llama-index after incorporating the documentation provided:
+
 ```python
 from gpt_index import (
     GPTTreeIndex,
@@ -10,27 +12,9 @@ from gpt_index import (
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 from gpt_index.evaluation import ResponseEvaluator
+from llama_index import SimpleMongoReader
 import pandas as pd
 pd.set_option('display.max_colwidth', 0)
-
-# Update: Import and install required packages
-import sys
-import os
-
-def install_packages(package_list):
-    for package in package_list:
-        try:
-            exec(f"import {package}")
-        except ImportError:
-            os.system(f"pip install {package}")
-
-# Check Python version, and install the appropriate dependencies
-if sys.version_info >= (3, 9):
-    install_requires = ["tiktoken"]
-else:
-    install_requires = ["transformers"]
-
-install_packages(install_requires)
 
 # gpt-3 (davinci)
 llm_predictor_gpt3 = LLMPredictor(llm=OpenAI(
@@ -51,6 +35,21 @@ vector_index.save_to_disk('../test_wiki/simple_vector_index.json')
 
 vector_index = GPTSimpleVectorIndex.load_from_disk(
     '../test_wiki/simple_vector_index.json')
+
+# Mongo Integration
+host = "<host>"
+port = "<port>"
+db_name = "<db_name>"
+collection_name = "<collection_name>"
+query_dict = {}
+reader = SimpleMongoReader(host, port)
+mongo_documents = reader.load_data(db_name, collection_name, query_dict=query_dict)
+
+# Updating index with Mongo documents
+tree_index.update_from_documents(mongo_documents)
+
+# Save updated index
+tree_index.save_to_disk('../test_wiki/updated_index.json')
 ```
 
-In the updated code, I have incorporated the necessary package installation process based on the given documentation. The code now checks for the required packages and installs them if they are not already installed. This ensures that the gpt-index / llama-index code runs smoothly on the host computer, regardless of the installed Python version.
+In the above-updated code, I have added the necessary imports from the documentation and incorporated the MongoDB integration. The `SimpleMongoReader` is used to read documents from the specified MongoDB collection. Then, the tree index is updated with these Mongo documents using the `update_from_documents` method. Finally, the updated index is saved to disk. Remember to replace "<host>", "<port>", "<db_name>", "<collection_name>", and "<query_text>" with your own specific values.
